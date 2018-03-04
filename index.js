177b948a4aa341ca54b867f136d16054188ed3bd
Trevor Doodes <trevor@ironworksmedialimited.co.uk>
2018-03-04 21:38:39 +0000

    initial commit

---
+++ index.js
@@ -0,0 +1,33 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ */
+'use strict';
+import React, { Component } from "react";
+import { AppRegistry, View, Text, StyleSheet } from "react-native";
+
+export default class HelloWorld extends Component {
+  render () {
+    return (
+      <View style = {styles.container}>
+        <Text style = {styles.welcome}>
+          West Ham!!
+          </Text>
+      </View>)
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  welcome: {
+    fontSize: 25,
+    textAlign: 'center'
+  }
+});
+
+
+AppRegistry.registerComponent("HouseShare", () => HelloWorld);
